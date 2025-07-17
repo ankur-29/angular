39fcb565a1f876f1ab3bd4022e60c481a00b73a9
ankur-29 <ankur.anand.me044@gmail.com>
2025-07-18 00:08:41 +0530

    components

---
+++ my-tutorial-app/src/app/admin/admin.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  imports: [],
+  templateUrl: './admin.html',
+  styleUrl: './admin.css'
+})
+export class Admin {
+  firstName = "Ankur";
+  courseName = "Angular_tutorial v20";
+}
